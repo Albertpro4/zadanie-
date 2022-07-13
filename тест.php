bdc30dfc00fee4518d49819e00cfd2b98b655d80
Albert <93717553+Albertpro4@users.noreply.github.com>
2022-07-13 04:39:03 +0300

    Add files via upload

---
+++ тест.php
@@ -0,0 +1,151 @@
+<?php
+
+   // Для начала необходимо, создать общий класс животных, от которого будут взаимствовать другие животные
+
+class Animal{
+    static $id  = 1; 
+
+    public $idAnimal = 0;
+
+    public $product;
+
+    protected function GetFromProduct(){}
+
+    public function getNameOfClass()
+
+    {
+        return static::class;
+    }
+}
+
+
+
+class Cow extends Animal{
+
+
+    function __construct(){
+        $this->idAnimal = parent::$id++;
+        $this->product = "Молоко";
+    }
+
+    function GetFromProduct()
+    {
+        return rand(8,12);
+    }
+}
+
+class Chicken extends Animal{
+    function __construct()
+    {
+        $this->idAnimal = parent::$id++;
+        $this->product = "Яйца";
+    }
+
+    function GetFromProduct()
+    {
+        return rand(0,1);
+    }
+}
+
+// Необходимо создать класс ферму, как написано в задании и внести данные
+
+
+class Farm{
+
+    private $an = array();
+    private $result;
+
+    public function AddingAnimal($animal){
+        array_push($this->an[$animal->getNameOfClass()], $animal);
+    }
+   
+    public function CountAnimals(){
+        echo "Количество животных " . '</br>';
+        foreach ($this->an as $animal => $value) {
+            echo "$animal: " . count($value) . '</br>';
+        }
+    }
+
+    
+
+
+    public function GetsomeProductionOnWeekly(){
+
+        for ($i=0; $i < 7; $i++) { 
+            $this->GetsomeProductionOnDaily();
+        }
+
+        echo "Произведенная продукция за неделю " . '</br>' ;
+      
+        foreach ($this->result as $type => $count) {
+            echo  $type . ": " . $count ;
+        }
+       
+    }
+  
+    public function AddAnimal($animal){
+        $this->an[$animal->getNameOfClass()] = array();
+    }
+    public function GetsomeProductionOnDaily(){
+
+      
+        foreach ($this->an as $Animal => $value) {
+        
+            $form = $value[0]->product;
+
+
+
+            $sum = 0;
+
+
+         
+            $this->result[$form] = $sum;
+
+          
+            foreach ($value as $animal) {
+
+
+                $sum += $animal->GetFromProduct();
+
+
+            }
+
+
+            $this->result[$form] += $sum;
+        }
+
+    }
+}
+
+
+
+
+
+$farm = new Farm();
+
+$farm->AddAnimal(new Cow);
+for ($i = 0; $i < 10; $i++) { 
+    $farm->AddingAnimal(new Cow) ;
+}
+$farm->AddAnimal(new Chicken);
+for ($i = 0; $i < 20; $i++) { 
+    $farm->AddingAnimal(new Chicken);
+}
+
+$farm->CountAnimals();
+
+$farm->GetsomeProductionOnWeekly();
+
+echo "купили животных" . '</br>';
+for ($i = 0; $i < 5; $i++){ 
+    $farm->AddingAnimal(new Chicken);
+}
+
+for ($i = 0; $i < 1; $i++){ 
+  $farm->AddingAnimal(new Cow);
+}
+
+
+$farm->CountAnimals();
+
+$farm->GetsomeProductionOnWeekly();
